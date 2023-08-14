6a78bbef7e3b0c0255b95876a9ac979eff03678d
Muhammad Hamza Haneef Qureshi <muhammadhamzahaneef@gmail.com>
2023-08-14 15:31:16 +0500

    update : resume details

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Muhammad Hamza',
     title: "MERN Stack Developer",
-    desciption:"Experience is every thing to me, I saw there’s no small and moderate learning , learning is always helps you when you get stuck no matter either its small or its too big matter is its always helps you in most needed places.",
-    image: 'https://avatars.githubusercontent.com/u/26695351?s=400&u=1ede922fe8349ea830e85918319867a5ce265f48&v=4',
+    desciption:"Experience is my cornerstone, a guiding light through challenges of all sizes. I hold the belief that learning, whether big or small, becomes the beacon that illuminates the path when hurdles arise. Its true worth is evident in critical moments, providing the essential toolkit to navigate through any situation.",
+    image: 'https://scontent.fkhi2-2.fna.fbcdn.net/v/t39.30808-6/226435056_2114866218664593_3195602507912244272_n.jpg?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=be3454&_nc_eui2=AeFcnqCAMQCofUm7Jqi7j4vXwM6gW_jA6obAzqBb-MDqhgw5_IF0MaB4PhJY8uBYh4pVU_wXrlhYw_EZ0OZGwcQ_&_nc_ohc=r8S97jnyOq0AX-NJgs1&_nc_oc=AQlvb0lVE_yu9vATzD-tc1a7OrG9HDc3xOd3ZDT67-uDk84risJnpTtayRBCiYrTIBI&_nc_ht=scontent.fkhi2-2.fna&oh=00_AfBPMZQqEs0u57TV8IG0gyrelzi7wdw1wGzll5T7m6q0nQ&oe=64DE5D44',
     resumePdf: resume
 }
